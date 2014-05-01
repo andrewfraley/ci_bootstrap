76b18efaeb154be1992817c377b52649c201a11f
Andy Fraley <andrew.fraley@gmail.com>
2014-04-30 22:58:26 -0400

    fixed file permissions and short open tag issue

--- application/views/layout/content_template.php
+++ application/views/layout/content_template.php
@@ -4,4 +4,4 @@ $this->load->view('layout/header'); ?>
  <h1>Bootstrap starter template</h1>
       <p>Use this document as a way to quick start any new project.<br> All you get is this message and a barebones HTML document.</p>
 	  
-<? $this->load->view('layout/footer');
+<?php $this->load->view('layout/footer');
